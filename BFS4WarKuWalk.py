d8ff35813a905f125a5bdde2f1b35038a1f611e6
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-23 16:05:40 +0700

    MapCreator

--- BFS4WarKuWalk.py
+++ BFS4WarKuWalk.py
@@ -1,3 +1,26 @@
 '''
 @Author - ReiiYuki
 '''
+import random
+
+def randomMapCreate(size,obstacle) :
+    list = []
+    for i in range(0,size) :
+        list.append([])
+        for j in range(0,size) :
+            list[i].append(0)
+
+    for i in range(0,obstacle) :
+        x = random.randint(0,size-1)
+        y = random.randint(0,size-1)
+        list[x][y] = 1
+    return list
+
+def printMap(map) :
+    for i in map :
+        for j in i :
+            print (j,end = " ")
+        print()
+
+map = randomMapCreate(16,50)
+printMap(map)
